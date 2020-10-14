884bf172fdae864b1ec1cb26eeda3c3189036eec
itop55 <bachida.imihi@gmail.com>
2020-10-14 15:59:47 +0300

    add gh-pages

---
+++ build/precache-manifest.bd541ba23f7d7015f6bc20d4b64b05af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841d376b7e45ad40baf45d0560640aa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad043f0fda24c681500c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9fbcfdd17c8f401dd1a0",
+    "url": "/static/js/2.a328cada.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a328cada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad043f0fda24c681500c",
+    "url": "/static/js/main.aa260cf5.chunk.js"
+  },
+  {
+    "revision": "56b4cb33b6ca1fab2d43",
+    "url": "/static/js/runtime-main.f736e969.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
